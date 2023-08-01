dc4e7965a74ea8ca1e35f3c8e0de2605dbbde698
Alvaro Andres Lopez Arias <37458753+alan89202@users.noreply.github.com>
2023-08-01 14:20:41 +0200

    Create GCP_main_server.tf

---
+++ terraform/GCP_main_server.tf
@@ -0,0 +1,24 @@
+variable "GCP_CREDENTIALS" {
+  description = "Archivo de credenciales para Google Cloud Platform"
+}
+
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.GCP_CREDENTIALS)
+
+  project = "<PROJECT_ID>"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
